34c908133e9d456d21396de4346e1f86d6cba7ef
Dmitry Pavlov <mitrickx@mail.ru>
2023-12-24 17:48:35 +0300

    HW2: modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,66 @@
-pub const VEC3_LEN: usize = 3;
-
-pub type Pair = (i32, i32);
-pub type Vec3 = [i32; VEC3_LEN];
-pub type SignedCounter = isize;
-pub type UnsignedCounter = usize;
-
-pub fn default_signed_counter() -> SignedCounter {
-    0
-}
-
-pub fn default_unsigned_counter() -> UnsignedCounter {
-    0
-}
-
-pub fn default_vec3() -> Vec3 {
-    [0; 3]
-}
-
-pub fn default_pair() -> Pair {
-    (0, 0)
-}
-
-pub fn next_signed(counter: SignedCounter) -> SignedCounter {
-    counter + 1
-}
-
-pub fn next_unsigned(counter: UnsignedCounter) -> UnsignedCounter {
-    counter + 1
-}
-
-pub fn prev_signed(counter: SignedCounter) -> SignedCounter {
-    counter - 1
-}
-
-pub fn vec3_vector_sum(a: Vec3, b: Vec3) -> Vec3 {
-    let mut c = default_vec3();
-    for i in 0..3 {
-        c[i] = a[i] + b[i];
+pub mod counter {
+    pub mod signed {
+        pub type Signed = isize;
+        pub fn default() -> Signed {
+            0
+        }
+        pub fn next(counter: Signed) -> Signed {
+            counter + 1
+        }
+        pub fn prev(counter: Signed) -> Signed {
+            counter - 1
+        }
     }
-    c
-}
 
-pub fn pair_vector_sum(a: Pair, b: Pair) -> Pair {
-    (a.0 + b.0, a.1 + b.1)
-}
-
-pub fn vec3_scalar_sum(a: Vec3, b: Vec3) -> i32 {
-    let mut c = 0;
-    for i in 0..VEC3_LEN {
-        c += a[i] + b[i];
+    pub mod unsigned {
+        pub type Unsigned = usize;
+        pub fn default() -> Unsigned {
+            0
+        }
+        pub fn next(counter: Unsigned) -> Unsigned {
+            counter + 1
+        }
     }
-    c
 }
 
-pub fn pair_scalar_sum(a: Pair, b: Pair) -> i32 {
-    a.0 + a.1 + b.0 + b.1
+pub mod pair {
+    pub type Pair = (i32, i32);
+
+    pub fn default() -> Pair {
+        (0, 0)
+    }
+
+    pub fn vector_sum(a: Pair, b: Pair) -> Pair {
+        (a.0 + b.0, a.1 + b.1)
+    }
+
+    pub fn scalar_sum(a: Pair, b: Pair) -> i32 {
+        a.0 + a.1 + b.0 + b.1
+    }
+}
+
+pub mod vec {
+    const LEN: usize = 3;
+
+    pub type Vec3 = [i32; LEN];
+
+    pub fn default() -> Vec3 {
+        [0; 3]
+    }
+
+    pub fn vector_sum(a: Vec3, b: Vec3) -> Vec3 {
+        let mut c = default();
+        for i in 0..3 {
+            c[i] = a[i] + b[i];
+        }
+        c
+    }
+
+    pub fn scalar_sum(a: Vec3, b: Vec3) -> i32 {
+        let mut c = 0;
+        for i in 0..LEN {
+            c += a[i] + b[i];
+        }
+        c
+    }
 }
